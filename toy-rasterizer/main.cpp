95ae0148e35ea5532c6a282f8985bff79cafe3ee
E-SeongHo <lsh031255@naver.com>
2022-02-07 17:45:41 +0900

    fix bug in line function

--- toy-rasterizer/main.cpp
+++ toy-rasterizer/main.cpp
@@ -5,11 +5,24 @@ const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red = TGAColor(255, 0, 0, 255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+    bool steep = false;
+    if (std::abs(x0 - x1) < std::abs(y0 - y1))
+    {   // transpose to make dx > dy
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+    if (x0 > x1)
+    {   // change p1 <-> p2 to start from left
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
     for (int x = x0; x <= x1; x++)
     {
         float t = (x - x0) / (float)(x1 - x0);
         int y = y0 * (1.0 - t) + y1 * t;
-        image.set(x, y, color);
+        if (steep) image.set(y, x, color); // de-transpose
+        else image.set(x, y, color);
     }
 }
 
@@ -19,6 +32,6 @@ int main(int argc, char** argv) {
     line(20, 13, 40, 80, image, red);
     line(80, 40, 13, 20, image, red);
     image.flip_vertically(); 
-    image.write_tga_file("output2.tga");
+    image.write_tga_file("output3.tga");
     return 0;
 }
